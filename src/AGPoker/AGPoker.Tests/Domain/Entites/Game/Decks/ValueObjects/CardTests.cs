22472d7517308355841ad1a9fae99906bf02c90b
AGranosik <adrian.granosik@gmail.com>
2022-12-27 09:15:41 +0100

    Cards

---
+++ src/AGPoker/AGPoker.Tests/Domain/Entites/Game/Decks/ValueObjects/CardTests.cs
@@ -0,0 +1,74 @@
+﻿using AGPoker.Entites.Game.Decks.ValueObjects;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AGPoker.Tests.Domain.Entites.Game.Decks.ValueObjects
+{
+    [TestFixture]
+    internal class CardTests
+    {
+        [Test]
+        [TestCase('A')]
+        [TestCase('B')]
+        [TestCase('E')]
+        [TestCase('F')]
+        [TestCase('G')]
+        [TestCase('A')]
+        [TestCase('Y')]
+        [TestCase('A')]
+        public void Card_NotValidSymbol_ThrowsException(char notValidSymbol)
+        {
+            var func = () => new Card(notValidSymbol, "3");
+            func.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        [TestCase('D')]
+        [TestCase('H')]
+        [TestCase('S')]
+        [TestCase('C')]
+        public void Card_AllValidSymbol_ThrowsException(char validSymbol)
+        {
+            var func = () => new Card(validSymbol, "3");
+            func.Should().NotThrow();
+        }
+
+        [Test]
+        [TestCase("1")]
+        [TestCase("Z")]
+        [TestCase("F")]
+        [TestCase("E")]
+        [TestCase("D")]
+        [TestCase("C")]
+        [TestCase("B")]
+        [TestCase("14")]
+        [TestCase("13")]
+        [TestCase("12")]
+        [TestCase("11")]
+        public void Card_NotAllValidValues_ThrowsException(string notValidValue)
+        {
+            var func = () => new Card('D', notValidValue);
+            func.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        [TestCase("A")]
+        [TestCase("K")]
+        [TestCase("Q")]
+        [TestCase("J")]
+        [TestCase("10")]
+        [TestCase("9")]
+        [TestCase("8")]
+        [TestCase("7")]
+        [TestCase("6")]
+        [TestCase("5")]
+        [TestCase("4")]
+        [TestCase("3")]
+        [TestCase("2")]
+        public void Card_AllValidValues_ThrowsException(string validValue)
+        {
+            var func = () => new Card('D', validValue);
+            func.Should().NotThrow();
+        }
+    }
+}
---
+++ src/AGPoker/AGPoker/Entites/Game/Decks/ValueObjects/Card.cs
@@ -0,0 +1,33 @@
+﻿namespace AGPoker.Entites.Game.Decks.ValueObjects
+{
+    public class Card
+    {
+        // D -> Diamond
+        // H -> Heart
+        // S -> Spade
+        // C -> Club
+
+        private static readonly List<char> _symbols = new() { 'D', 'H', 'S', 'C' };
+        private static readonly List<string> _values = new()
+        {
+            "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"
+        };
+        public Card(char symbol, string value)
+        {
+            Validation(symbol, value);
+            Symbol = symbol;
+            Value = value;
+        }
+        public char Symbol { get; init; }
+        public string Value { get; init; }
+
+        private void Validation(char symbol, string value)
+        {
+            if (!_symbols.Contains(symbol))
+                throw new ArgumentException(nameof(symbol));
+
+            if(!_values.Contains(value))
+                throw new ArgumentException(nameof(value));
+        }
+    }
+}
