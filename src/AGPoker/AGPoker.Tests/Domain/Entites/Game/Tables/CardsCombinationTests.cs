125ee1d7742895eda9f1bc8ecdfa45349258c2e2
AGranosik <adrian.granosik@gmail.com>
2023-03-26 14:58:30 +0200

    cards combination

--- src/AGPoker/AGPoker.Tests/Domain/Entites/Game/Tables/CardsCombinationTests.cs
+++ src/AGPoker/AGPoker.Tests/Domain/Entites/Game/Tables/CardsCombinationTests.cs
@@ -1,9 +1,73 @@
-﻿using NUnit.Framework;
+﻿using AGPoker.Entites.Game.Decks.ValueObjects;
+using AGPoker.Entites.Game.Tables.ValueObjects;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace AGPoker.Tests.Domain.Entites.Game.Tables
 {
     [TestFixture]
     internal class CardsCombinationTests
     {
+
+        [Test]
+        public void CardsCombination_CardsCannotBeNull_ThrowsException()
+        {
+            var func = () => CardsCombination.GetCombination(null);
+            func.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void CardsCombination_CardsCannotBeEmpty_ThrowsException()
+        {
+            var func = () => CardsCombination.GetCombination(new List<Card>());
+            func.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void CardsCombination_NoLessThan7Cards_ThrowsException()
+        {
+            var cards = new List<Card>
+            {
+                new Card('D', ECardValue.Three),
+                new Card('D', ECardValue.Four),
+                new Card('D', ECardValue.Five),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Seven)
+            };
+            var func = () => CardsCombination.GetCombination(cards);
+            func.Should().Throw<InvalidOperationException>();
+        }
+
+
+        [Test]
+        public void CardsCombination_NoMoreThan7Cards_ThrowsException()
+        {
+            var cards = new List<Card>
+            {
+                new Card('D', ECardValue.Three),
+                new Card('D', ECardValue.Four),
+                new Card('D', ECardValue.Five),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Seven)
+            };
+            var func = () => CardsCombination.GetCombination(cards);
+            func.Should().Throw<InvalidOperationException>();
+        }
+        [Test]
+        public void CardsCombination_StraightFlush_Success()
+        {
+            var cards = new List<Card>
+            {
+                new Card('D', ECardValue.Three),
+                new Card('D', ECardValue.Four),
+                new Card('D', ECardValue.Five),
+                new Card('D', ECardValue.Six),
+                new Card('D', ECardValue.Seven),
+                new Card('D', ECardValue.Eight)
+            };
+        }
     }
 }
--- src/AGPoker/AGPoker/Entites/Game/Tables/ValueObjects/CardsCombination.cs
+++ src/AGPoker/AGPoker/Entites/Game/Tables/ValueObjects/CardsCombination.cs
@@ -2,15 +2,56 @@
 
 namespace AGPoker.Entites.Game.Tables.ValueObjects
 {
-    internal class CardsCombination
+    public static class CardsCombination
     {
-        public void GetCOmbination(List<Card> cards)
+        public static CardResult GetCombination(List<Card> cards)
         {
+            CardsValidation(cards);
+            var isStraight = IsStraight(cards);
+            var isFLush = IsFlush(cards);
 
+            if (isStraight && isFLush)
+                return new CardResult(Combination.StraightFlush, ECardValue.Four);
+
+            return null;
         }
+
+        private static void CardsValidation(List<Card> cards)
+        {
+            if(cards is null)
+                throw new ArgumentNullException(nameof(cards));
+
+            if (cards.Count != 7)
+                throw new InvalidOperationException();
+        }
+
+        private static bool IsStraight(List<Card> cards)
+        {
+            var orderedByValue = cards.OrderByDescending(x => x.Value).ToList();
+            int cardsInOrder = 0;
+            for(int i = 0; i < orderedByValue.Count -1; i++)
+            { 
+                var currentCard = orderedByValue[i];
+                var nextCard = orderedByValue[i + 1];
+
+                if ((currentCard.Value - nextCard.Value) == 1)
+                    cardsInOrder++;
+                else
+                    cardsInOrder = 0;
+            }
+
+            return cardsInOrder >= 5;
+        }
+
+        private static bool IsFlush(List<Card> cards)
+            => cards.GroupBy(c => c.Symbol)
+                .Any(s => s.Count() >= 5);
+
+        private static ECardValue GetHighestValue(List<Card> cards)
+            => cards.Max(c => c.Value);
     }
 
-    internal class CardResult
+    public class CardResult
     {
         public CardResult(Combination combination, ECardValue highestCard)
         {
@@ -22,7 +63,7 @@ namespace AGPoker.Entites.Game.Tables.ValueObjects
         public ECardValue HighestCard { get; init; }
     }
 
-    internal enum Combination
+    public enum Combination
     {
         StraightFlush,
         FourOfKind,
