f8a53ecc49a754ec680dcb648c8f2ba6c3659850
AGranosik <adrian.granosik@gmail.com>
2023-04-25 13:03:27 +0200

    turn result

---
+++ src/AGPoker/AGPoker.Tests/Domain/Entites/Game/Turns/TurnResultTests.cs
@@ -0,0 +1,47 @@
+﻿using AGPoker.Entites.Game.Game.Players;
+using AGPoker.Entites.Game.Turns;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AGPoker.Tests.Domain.Entites.Game.Turns
+{
+    [TestFixture]
+    internal class TurnResultTests
+    {
+        [Test]
+        public void Creation_InProgress_Success()
+        {
+            var creatioNResult = TurnResult.InProgress();
+            creatioNResult.Should().NotBeNull();
+            creatioNResult.Status.Should().Be(TurnStatus.InProgress);
+        }
+
+        [Test]
+        public void Creation_WinnersCannotBeNull_ThrowsException()
+        {
+            var creationFunc = () => TurnResult.Winners(null);
+            creationFunc.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Creation_WinnerCannotBeEmpty_ThrowsException()
+        {
+            var creationFunc = () => TurnResult.Winners(new List<Player>());
+            creationFunc.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Creation_Winners_Success()
+        {
+            var winners = new List<Player>()
+            {
+                Player.Create("hehe", "dsfgsdf")
+            };
+            var creationFunc = TurnResult.Winners(winners);
+            creationFunc.Should().NotBeNull();
+
+            creationFunc.Status.Should().Be(TurnStatus.Winners);
+            creationFunc.WinnerPlayers.Should().BeEquivalentTo(winners);
+        }
+    }
+}
---
+++ src/AGPoker/AGPoker/Entites/Game/Turns/TurnResult.cs
@@ -0,0 +1,40 @@
+﻿using AGPoker.Entites.Game.Game.Players;
+
+namespace AGPoker.Entites.Game.Turns
+{
+    public class TurnResult
+    {
+        private TurnResult (TurnStatus status, List<Player> playersStayedInGame)
+        {
+            Validation(playersStayedInGame);
+            Status = status;
+            WinnerPlayers = playersStayedInGame;
+        }
+
+        private TurnResult(TurnStatus status)
+        {
+            Status = status;
+        }
+
+        public TurnStatus Status { get; init; }
+        public List<Player> WinnerPlayers { get; init; }
+
+        public static TurnResult InProgress()
+            => new(TurnStatus.InProgress);
+
+        public static TurnResult Winners(List<Player> playersStayedInGame)
+            => new(TurnStatus.Winners, playersStayedInGame);
+
+        private void Validation(List<Player> playersStayedInGame)
+        {
+            if(playersStayedInGame is null || playersStayedInGame.Count == 0)
+                throw new ArgumentException(nameof(playersStayedInGame));
+        }
+    }
+
+    public enum TurnStatus
+    {
+        InProgress,
+        Winners
+    }
+}
